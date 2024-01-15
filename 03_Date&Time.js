b81258291db88342fcb81cd569bed64c246f8f8c
Ujjwal Bhardwaj <118505645+ujjwal0011@users.noreply.github.com>
2024-01-15 19:15:06 +0530

    Update 03_Date&Time.js

--- 03_Date&Time.js
+++ 03_Date&Time.js
@@ -1,16 +1,17 @@
  // Date : Date is an object in javascript. 
+
 // JavaScript stores dates as number of milliseconds since January 01, 1970
+
 const myDate = new Date();
-console.log(myDate) //Output : 2023-10-09T14:38:09.847Z
-// We can use various methods to convert this date into more readable value.
-console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+console.log(myDate) //Output : 2024-01-15T12:53:19.374Z
 
-console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
-console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
-console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+// We can use various methods to convert this date into more readable value:
 
-console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
-console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+console.log(myDate.toString()) //Output : Mon Jan 15 2024 18:23:19 GMT+0530 (India Standard Time)
 
-console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
-console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
+console.log(myDate.toLocaleString()) // Output :15/1/2024, 6:23:19 pm
+console.log(myDate.toLocaleDateString()) //Output : 15/1/2024
+console.log(myDate.toLocaleTimeString()) // Output : 6:23:19 pm
+
+console.log(myDate.toDateString()) //Output : Mon Jan 15 2024
+console.log(myDate.toTimeString()) // Output : 18:23:19 GMT+0530 (India Standard Time)
