71c70c947205c499ac32ea05ce538dd6e7b166cf
Ujjwal Bhardwaj <118505645+ujjwal0011@users.noreply.github.com>
2024-01-17 07:39:27 +0530

    Add files via upload

---
+++ 05_objects.js
@@ -0,0 +1,26 @@
+let sym = Symbol("Hello")
+let user = {
+    Name: "Ujjwal",
+    age: 19,
+    year: 3,
+    [sym]: "World"          // correct way to define symbol in objects 
+}
+
+console.log(user.Name);     
+console.log(user["Name"]);  // a good practice to access objects
+
+console.log(user[sym]);     // accessing a symbol from objects
+
+user.Name = "Bhardwaj"   // changing values in objects
+// Object.freeze(user)         // freezing the object so that values can't be changed
+user.Name = "Ujjwal"     // this value will not be changed now
+
+console.log(user);
+
+// adding a function to an object
+
+user.greeting = function(){  
+    console.log(`Hello JS User, ${this.Name}`);  // for referencing same object, we use this keyword
+}
+
+console.log(user.greeting());  // accessing a function from object
