a18161981af7789056a79bd2a0a7307d1e4f9b14
Ujjwal Bhardwaj <118505645+ujjwal0011@users.noreply.github.com>
2024-01-24 20:37:26 +0530

    Update 12_loops.js

--- 12_loops.js
+++ 12_loops.js
@@ -4,7 +4,7 @@ let myArr = [3, 1, 8, 4, 5, 2, 9, 6, 10, 7]
 
 for(let i = 0; i<=myArr.length; i++){
     if(myArr[i] % 2 == 0){
-        console.log(`Even number ${myArr[i]} is present at index ${i}`);
+        // console.log(`Even number ${myArr[i]} is present at index ${i}`);
     }
 }
 
@@ -12,7 +12,7 @@ for(let i = 0; i<=20; i++){
     if(i % 2 == 0){
         continue
     }
-    console.log(i);
+    // console.log(i);
 }
 
 
@@ -23,7 +23,7 @@ let myArray = ['Toji', "Yuji", "Megumi"]
 
 let arr = 0
 while (arr < myArray.length) {
-    console.log(`Value is ${myArray[arr]}`);
+    // console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
 
@@ -34,7 +34,7 @@ while (arr < myArray.length) {
 let i = 1
 
 do {
-    console.log(`Value of i is ${i}`);
+    // console.log(`Value of i is ${i}`);
     i++
 } while (i <= 10);
 
@@ -42,6 +42,37 @@ do {
 let score = 11
 
 do {
-    console.log(`Score is ${score}`);
+    // console.log(`Score is ${score}`);
     score++
 } while (score <= 10);
+
+
+// --------------------------- For of and For in ------------------------
+
+
+let newArr = [1, 2, 3, 4, 5]
+
+for (const i of newArr) {
+    // console.log(i);
+}
+
+
+let greetings = "Hello World"
+
+for(const greet of greetings){
+    // console.log(greet);
+}
+
+
+let map = new Map()
+map.set('Jujutsu', 'Kaisen')
+map.set('Attack on', 'Titan')
+map.set('Solo', 'Leveling')
+map.set('Demon', 'Slayer')
+
+for(const [key, value] of map){
+    // console.log(`${key} => ${value}`);
+}
+
+
+
